f3bead409f67e4aaf74a178a6aba51dd42da6042
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-11-02 12:01:19 -0700

    it flies

---
+++ assets/conv.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+from PIL import Image
+
+
+def rp6502_rgb(r, g, b):
+    if r == 0 and g == 0 and b == 0:
+        return 0
+    else:
+        return (((b >> 3) << 11) | ((g >> 3) << 6) | (r >> 3)) | 1 << 5
+
+
+def get_palette():
+    with Image.new(mode="RGB", size=[10, 10]) as im:
+        with open("bin/palette.bin", "wb") as o:
+            im2 = im.convert("P", palette=Image.WEB, colors=256)
+            pal = im2.getpalette()
+            for r, g, b in zip(*[iter(pal)] * 3):
+                o.write(
+                    rp6502_rgb(r, g, b).to_bytes(2, byteorder="little", signed=False)
+                )
+    return pal
+
+
+def conv_img(name_in, name_out):
+    with Image.open(name_in) as im:
+        # im2 = im.convert("P", palette=palette, colors=256)
+        # im = im.convert("RGB")
+        im2 = im.convert("P", palette=Image.ADAPTIVE, colors=256)
+        # im2 = im.quantize(palette=palette)
+        with open("bin/" + name_out + ".dat.bin", "wb") as o:
+            for y in range(0, int(im2.height / 2)):
+                for x in range(0, int(im2.width / 2)):
+                    o.write(
+                        im2.getpixel((x * 2, y * 2)).to_bytes(
+                            1, byteorder="little", signed=False
+                        )
+                    )
+        with open("bin/" + name_out + ".pal.bin", "wb") as o:
+            for r, g, b in zip(*[iter(im2.getpalette())] * 3):
+                o.write(
+                    rp6502_rgb(r, g, b).to_bytes(2, byteorder="little", signed=False)
+                )
+
+
+def conv_spr(name_in, name_out):
+    with Image.open(name_in) as im:
+        with open("bin/" + name_out, "wb") as o:
+            rgb_im = im.convert("RGB")
+            im2 = rgb_im.resize(size=[16, 16])
+            for y in range(0, im2.height):
+                for x in range(0, im2.width):
+                    r, g, b = im2.getpixel((x, y))
+                    o.write(
+                        rp6502_rgb(r, g, b).to_bytes(
+                            2, byteorder="little", signed=False
+                        )
+                    )
+
+
+# palette = get_palette()
+
+conv_img("samuelcust/sprites/background-day.png", "bg_day_144x256")
+
+conv_spr("samuelcust/sprites/bluebird-midflap.png", "midflap_spr16.bin")
--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,62 @@
 #include <rp6502.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+void task()
+{
+    static int x = 0;
+    static int y = 100;
+
+    xram0_struct_set(0xFF10, vga_mode3_config_t, x_pos_px, x);
+    if (--x < 0)
+        x = 143;
+
+    RIA.addr0 = 0xFFE0;
+    if (!(RIA.rw0 & 1))
+    {
+        if (--y < 0)
+            y = 0;
+    }
+    else
+    {
+        if (++y > 200)
+            y = 200;
+    }
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, y_pos_px, y);
+}
 
 void main()
 {
-    puts("Hello, world!");
+    xreg_vga_canvas(1);
+
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, x_pos_px, 10);
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, y_pos_px, 100);
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, xram_sprite_ptr, 0x0000);
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, log_size, 4);
+    xram0_struct_set(0xFF00, vga_mode4_sprite_t, has_opacity_metadata, false);
+    xreg_vga_mode(4, 0, 0xFF00, 1, 0);
+
+    xram0_struct_set(0xFF10, vga_mode3_config_t, x_wrap, true);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, y_wrap, true);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, x_pos_px, 0);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, y_pos_px, 0);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, width_px, 144);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, height_px, 256);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, xram_data_ptr, 0x2000);
+    xram0_struct_set(0xFF10, vga_mode3_config_t, xram_palette_ptr, 0x1000);
+    xreg_vga_mode(3, 3, 0xFF10, 0);
+
+    xreg_ria_keyboard(0xFFE0);
+
+    {
+        uint8_t v = RIA.vsync;
+        while (1)
+        {
+            if (RIA.vsync == v)
+                continue;
+            v = RIA.vsync;
+            task();
+        }
+    }
 }
